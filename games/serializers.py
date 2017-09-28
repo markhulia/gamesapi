08a485814bde42e699d41926d64ac29dc1a3658c
pumba <markhulia@users.noreply.github.com>
2017-09-28 16:37:49 +0200

    API views
    Simple API views for CRUD operations

--- games/serializers.py
+++ games/serializers.py
@@ -16,11 +16,8 @@ class GameSerializer(serializers.Serializer):
         instance.name = validated_data.get('name', instance.name)
         instance.release_date = validated_data.get('release_date', instance.release_date)
         instance.game_category = validated_data.get('game_category', instance.game_category)
-
-
-
-
-        #>>>>>>>>>>>>>> MISSPELLED player
         instance.played = validated_data.get('played', instance.played)
         instance.save()
         return instance
+
+
